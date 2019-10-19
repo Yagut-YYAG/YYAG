a7c1e65d532654333ad8a76e412a714012288dcd
Dell_y <yagut.badalova@gmail.com>
2019-10-19 05:35:05 +0500

    ggg

---
+++ src/main/java/hw2/code_1.java
@@ -0,0 +1,8 @@
+package hw2;
+
+public class code_1 {
+    public static void main(String[] args) {
+        hyjfgj s=new hyjfgj();
+        s.print();
+    }
+}
---
+++ src/main/java/hw2/hyjfgj.java
@@ -0,0 +1,7 @@
+package hw2;
+
+public class hyjfgj {
+    public static void print(){
+        System.out.println("hello world");
+    }
+}
