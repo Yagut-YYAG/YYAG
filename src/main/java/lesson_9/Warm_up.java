d39c3387ddfe340d74a4e18a391dcc5a35f1c6d7
vafamustafayeva <vafamustafayeva4@gmail.com>
2019-10-30 21:36:48 +0400

    lesson_9

---
+++ src/main/java/lesson_9/Warm_up.java
@@ -0,0 +1,14 @@
+package lesson_9;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Warm_up {
+    public static void main(String[] args) {
+        ArrayList<String> str = new ArrayList<>();
+        File my_file= new File("hello.txt");
+        my_file.canWrite();
+
+    }
+}
